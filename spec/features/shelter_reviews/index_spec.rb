f782410462ae0fdfff43e4f838c67b73c3443d35
twarbelow <taija.warbelow@google.com>
2020-07-06 16:47:31 -0700

    half complete shelter review index test

---
+++ spec/features/shelter_reviews/index_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe "shelter reviews index" do
+  it "shows user list of reviews for specific shelter on shelter show page" do
+    shelter_1 = Shelter.create(name:       "Good Boys Are Us",
+                               address:     "1234 ABC Street",
+                               city:        "Denver",
+                               state:       "Colorado",
+                               zip:         80202)
+
+    review_1 = Review.create(title:     "Best Review",
+                             rating:    5,
+                             content:   "Such good pets, all the best.",
+                             picture:   "this is a pic")
+
+    visit "shelters/#{shelter_1.id}"
+
+    expect(page).to have_content(review_1.title)
+    expect(page).to have_content(review_1.rating)
+    expect(page).to have_content(review_1.content)
+    expect(page).to have_content(review_1.picture)
+  end
+end
