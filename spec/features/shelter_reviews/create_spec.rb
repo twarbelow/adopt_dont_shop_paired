91c3bdb01bc02e84e86a30a3b003397b70415d24
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-07-06 19:34:57 -0600

    Update review create spec to include image_path

--- spec/features/shelter_reviews/create_spec.rb
+++ spec/features/shelter_reviews/create_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "shelter review create page" do
   it "allows user to create a review for this shelter" do
-    shelter_1 = Shelter.create(name:       "Good Boys Are Us",
+    shelter_1 = Shelter.create(name:        "Good Boys Are Us",
                                address:     "1234 ABC Street",
                                city:        "Denver",
                                state:       "Colorado",
@@ -17,7 +17,7 @@ RSpec.describe "shelter review create page" do
     fill_in :title,           with: "Cute little place!"
     fill_in :rating,          with: 4 # <------ Dependant on db, integer or string. also, select('4', :from => 'Rating') for dropdown?
     fill_in :content,         with: "Omg love this place I go here all the time"
-    fill_in :optional_image:, with: "https://i.redd.it/jab5c3kfm8s21.jpg"
+    fill_in :image_path,      with: "https://i.redd.it/jab5c3kfm8s21.jpg"
 
     click_on "Submit Review"
 
