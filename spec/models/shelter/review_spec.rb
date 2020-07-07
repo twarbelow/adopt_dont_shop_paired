d5e24590081509fcd28e552454dea1f1254df266
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-07-07 11:32:58 -0600

    Validate info for review model

---
+++ spec/models/shelter/review_spec.rb
@@ -0,0 +1,12 @@
+RSpec.describe Review do
+  describe 'validations' do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :rating }
+    it { should validate_presence_of :content }
+    it { should validate_presence_of :shelter }
+  end
+
+  describe 'relationships' do
+    it { should belong_to :shelter }
+  end
+end
