ad231602f02ee7a7b356e1a8df14f8422191d5dc
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-07-06 19:37:00 -0600

    Update review index spec to have image_path and inlcude notes

--- spec/features/shelter_reviews/index_spec.rb
+++ spec/features/shelter_reviews/index_spec.rb
@@ -9,10 +9,12 @@ RSpec.describe "shelter reviews index" do
                                state:       "Colorado",
                                zip:         80202)
 
+
+    # If we have a method in shelter that is review, can we call shelter.reviews.create?
     review_1 = Review.create(title:         "Best Review",
                              rating:        5,
                              content:       "Such good pets, all the best.",
-                             picture:       "https://www.northeastanimalshelter.org/wp-content/uploads/2013/10/1-IMG_8364-0011.jpg",
+                             image_path:    "https://www.northeastanimalshelter.org/wp-content/uploads/2013/10/1-IMG_8364-0011.jpg",
                              shelter_id:    shelter_1.id)
 
     visit "shelters/#{shelter_1.id}"
@@ -20,6 +22,6 @@ RSpec.describe "shelter reviews index" do
     expect(page).to have_content(review_1.title)
     expect(page).to have_content(review_1.rating)
     expect(page).to have_content(review_1.content)
-    expect(page).to have_content(review_1.picture)
+    expect(page).to have_content(review_1.image_path)
   end
 end
