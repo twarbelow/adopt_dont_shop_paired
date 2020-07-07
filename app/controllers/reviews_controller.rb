7b25dd8b99043f0b3c391503a9e2a4576d1fd33b
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-07-07 17:41:49 -0600

    Add edit and update methods to review controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,4 +7,15 @@ class ReviewsController < ApplicationController
   def edit
     @review = Review.find(params[:id])
   end
+
+  def update
+    review = Review.find(params[:id])
+    review.update(review_params)
+    redirect_to "/shelters/#{review.shelter.id}"
+  end
+
+  private
+  def review_params
+    params.permit(:title, :rating, :content, :image_path)
+  end
 end
