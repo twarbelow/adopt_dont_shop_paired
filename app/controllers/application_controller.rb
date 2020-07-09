9801db711f235490bb34ddec8bcd816fe96b0d07
twarbelow <taija.warbelow@google.com>
2020-07-09 16:12:30 -0700

    minor necessary changes for favorites spec

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action(:set_favorites)
+
+  def set_favorites
+    @favorites = Favorites.new(session[:favorites])
+  end
 end
