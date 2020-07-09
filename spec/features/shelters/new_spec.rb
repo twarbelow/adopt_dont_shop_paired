60f28af74801d5a264a1dd4e62949966176cf716
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-07-08 18:42:30 -0600

    Update new shelter spec

--- spec/features/shelters/new_spec.rb
+++ spec/features/shelters/new_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "shelter creation page", type: :feature do
+RSpec.describe "Shelter creation process", type: :feature do
   it "takes you to a form to create a new shelter" do
 
     visit '/shelters'
@@ -20,18 +20,3 @@ RSpec.describe "shelter creation page", type: :feature do
     page.has_content?('Good Boys Are Us')
   end
 end
-
-# When I visit the Shelter Index page
-# Then I see a link to create a new Shelter, "New Shelter"
-# When I click this link
-# Then I am taken to '/shelters/new' where I  see a form for a new shelter
-# When I fill out the form with a new shelter's:
-# - name
-# - address
-# - city
-# - state
-# - zip
-# And I click the button "Create Shelter" to submit the form
-# Then a `POST` request is sent to '/shelters',
-# a new shelter is created,
-# and I am redirected to the Shelter Index page where I see the new Shelter listed.
