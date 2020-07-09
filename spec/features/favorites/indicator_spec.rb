b24a75876b696c7c44fa588170377b291e136d10
twarbelow <taija.warbelow@google.com>
2020-07-09 12:24:05 -0700

    full outline indicator spec, tests not passing

--- spec/features/favorites/indicator_spec.rb
+++ spec/features/favorites/indicator_spec.rb
@@ -2,7 +2,30 @@ require 'rails_helper'
 
 RSpec.describe 'Favorites Indicator' do
   it 'is in the nav bar on every page' do
+    shelter = Shelter.create(name:         "Good Boys Are Us",
+                               address:       "1234 ABC Street",
+                               city:          "Denver",
+                               state:         "Colorado",
+                               zip:           80202)
+
+    pet = Pet.create(image:                "https://cdn0.wideopenpets.com/wp-content/uploads/2019/03/Untitled-design-2019-03-27T180734.510-770x405.jpg",
+                        name:                 "Sokka",
+                        approximate_age:      3.0,
+                        sex:                  "Male",
+                        shelter_id:           @shelter_1.id)
+
+    visit "/"
+    expect(page).to have_content("Favorites")
+
+    visit "/pets"
+    expect(page).to have_content("Favorites")
+
+    visit "/pets/#{pet.id}"
+    expect(page).to have_content("Favorites")
   end
+
   it 'show a count of favorites' do
+    visit "/" 
+    expect(page).to have_content("Favorites: 0")
   end
 end
