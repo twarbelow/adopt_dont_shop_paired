07ffe32d40e3f779b8b5ef65317ba6302b84e06d
twarbelow <taija.warbelow@google.com>
2020-07-08 17:13:13 -0700

    sad path spec for new review

--- spec/features/reviews/new_spec.rb
+++ spec/features/reviews/new_spec.rb
@@ -1,31 +1,53 @@
 require 'rails_helper'
 
-RSpec.describe "shelter review create page" do
-  it "allows user to create a review for this shelter" do
-    shelter_1 = Shelter.create(name:        "Good Boys Are Us",
-                               address:     "1234 ABC Street",
-                               city:        "Denver",
-                               state:       "Colorado",
-                               zip:         80202)
+RSpec.describe "new shelter review" do
+  describe 'As a visitor'
+    before: :each do
+      shelter_1 = Shelter.create(name:        "Good Boys Are Us",
+                                 address:     "1234 ABC Street",
+                                 city:        "Denver",
+                                 state:       "Colorado",
+                                 zip:         80202)
+      visit "shelters/#{shelter_1.id}"
+    end
 
-    visit "shelters/#{shelter_1.id}"
+      click_on 'Add Review'
+    it "I can create a review for this shelter" do
+      # shelter_1 = Shelter.create(name:        "Good Boys Are Us",
+      #                            address:     "1234 ABC Street",
+      #                            city:        "Denver",
+      #                            state:       "Colorado",
+      #                            zip:         80202)
+      #
+      # visit "shelters/#{shelter_1.id}"
+      #
+      # click_on 'Add Review'
+      expect(current_path).to eq("/shelters/#{shelter_1.id}/reviews/new")
 
-    click_on 'Add Review'
-    expect(current_path).to eq("/shelters/#{shelter_1.id}/reviews/new")
+      fill_in :title,           with: "Cute little place!"
+      select('4', from: :rating)
+      fill_in :content,         with: "Omg love this place I go here all the time"
+      fill_in :image_path,      with: "https://i.redd.it/jab5c3kfm8s21.jpg"
 
-    fill_in :title,           with: "Cute little place!"
-    select('4', from: :rating)
-    # fill_in :rating,          with: 4
-    fill_in :content,         with: "Omg love this place I go here all the time"
-    fill_in :image_path,      with: "https://i.redd.it/jab5c3kfm8s21.jpg"
+      click_on "Submit Review"
 
-    click_on "Submit Review"
+      expect(current_path).to eq("/shelters/#{shelter_1.id}")
 
-    expect(current_path).to eq("/shelters/#{shelter_1.id}")
+      review = Review.last
+      expect(page).to have_content(review.title)
+      expect(page).to have_content(review.rating)
+      expect(page).to have_content(review.content)
+    end
 
-    review = Review.last
-    expect(page).to have_content(review.title)
-    expect(page).to have_content(review.rating)
-    expect(page).to have_content(review.content)
-  end
+    it 'I cannot create a review for this shelter without a title, rating, and content' do
+      expect(current_path).to eq("/shelters/#{shelter_1.id}/reviews/new")
+
+      fill_in :title,           with: "Cute little place!"
+      fill_in :content,         with: "Omg love this place I go here all the time"
+      fill_in :image_path,      with: "https://i.redd.it/jab5c3kfm8s21.jpg"
+
+      click_on "Submit Review"
+
+      expect(page).to have_content("Review not submitted: Required information missing.")
+    end
 end
