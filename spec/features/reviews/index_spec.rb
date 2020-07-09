386fe9b1635a4828694ab72f210c80fa03c8ee13
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-07-09 16:27:13 -0600

    Update review index spec

--- spec/features/reviews/index_spec.rb
+++ spec/features/reviews/index_spec.rb
@@ -1,29 +1,36 @@
 require 'rails_helper'
 
-RSpec.describe "shelter reviews index" do
-  it "shows user list of reviews for specific shelter on shelter show page" do
+RSpec.describe "Shelter reviews index" do
+  before(:each) do
 
-    shelter_1 = Shelter.create(name:        "Good Boys Are Us",
-                               address:     "1234 ABC Street",
-                               city:        "Denver",
-                               state:       "Colorado",
-                               zip:         80202)
+    @shelter_1 = Shelter.create!(name:        "Good Boys Are Us",
+                                 address:     "1234 ABC Street",
+                                 city:        "Denver",
+                                 state:       "Colorado",
+                                 zip:         80202)
 
+    @review_1 = Review.create!(title:         "Best Review",
+                               rating:        5,
+                               content:       "Such good pets, all the best.",
+                               image_path:    "https://www.northeastanimalshelter.org/wp-content/uploads/2013/10/1-IMG_8364-0011.jpg",
+                               shelter_id:    @shelter_1.id)
 
+    visit "shelters/#{@shelter_1.id}"
+  end
 
-    review_1 = shelter_1.reviews.create(
-                             title:         "Best Review",
-                             rating:        5,
-                             content:       "Such good pets, all the best.",
-                             image_path:    "https://www.northeastanimalshelter.org/wp-content/uploads/2013/10/1-IMG_8364-0011.jpg",
-                             shelter_id:    shelter_1.id)
+  it "can display review title" do
+    expect(page).to have_content(@review_1.title)
+  end
 
-    visit "shelters/#{shelter_1.id}"
-    
-    # Everything works up until this point
-    expect(page).to have_content(review_1.title)
-    expect(page).to have_content(review_1.rating)
-    expect(page).to have_content(review_1.content)
+  it "can display review rating" do
+    expect(page).to have_content(@review_1.rating)
+  end
+
+  it "can display review content" do
+    expect(page).to have_content(@review_1.content)
+  end
+
+  it "can display review image" do
     expect(page).to have_css("img[src*='https://www.northeastanimalshelter.org/wp-content/uploads/2013/10/1-IMG_8364-0011.jpg']")
   end
 end
