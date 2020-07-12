394117462c07884925707ed90d7657c2c0297703
twarbelow <taija.warbelow@google.com>
2020-07-12 11:53:21 -0700

    delete unused files

--- spec/features/favorites/index_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe 'it shows a favorites index page' do
-  it ' when the favorites indicator is clicked' do
-    visit '/shelters'
-    find('.fav-pets').click
-    expect(current_path).to eq '/favorites'
-  end
-end
