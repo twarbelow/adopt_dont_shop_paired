45dc9d233f0956a953c13491a7414e8dced8c334
twarbelow <taija.warbelow@google.com>
2020-07-12 09:53:13 -0700

    clean up test

--- spec/features/favorites/index_spec.rb
+++ spec/features/favorites/index_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe 'it shows a favorites index page' do
   it ' when the favorites indicator is clicked' do
     visit '/shelters'
-
     find('.fav-pets').click
     expect(current_path).to eq '/favorites'
   end
