3da67b9ca540edd2edcf6f05d948d2459619d8a5
twarbelow <taija.warbelow@google.com>
2020-07-12 16:25:50 -0700

    add has_many relationship with apps

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -5,6 +5,8 @@ class Pet < ApplicationRecord
   validates_presence_of :sex
   validates_presence_of :shelter_id
   belongs_to :shelter
+  has_many :app_pets
+  has_many :apps, through: :app_pet
 
   def self.favorited(favorites)
     favorites
