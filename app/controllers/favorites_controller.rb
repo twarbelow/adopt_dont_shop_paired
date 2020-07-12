efac782824c779db99cad149e2a5faaa967ab9a1
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-07-12 09:58:03 -0600

    Add create method with flash method in favorites controller

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,4 +2,11 @@ class FavoritesController < ApplicationController
   def index
     @pets = Pet.all
   end
+
+  def create
+    @favorites.add(params[:id])
+    session[:favorites] = @favorites.favorite_pets
+    flash[:favorited] = "Pet has been added to your favorites list"
+    redirect_to "/pets/#{params[:id]}"
+  end
 end
