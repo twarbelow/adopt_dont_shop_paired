398ae83d6e084156761ace45fcf7903e462abaae
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-07-12 14:07:40 -0600

    Add method to find if pet is already favorited as well as a delete method

--- app/models/favorites.rb
+++ app/models/favorites.rb
@@ -15,4 +15,12 @@ class Favorites
   def sum_favorites
     @favorite_pets.count
   end
+
+  def is_already_favorited?(pet_id)
+    @favorite_pets.include?(pet_id.to_s)
+  end
+
+  def delete(pet_ids)
+    @favorite_pets.delete(pet_ids)
+  end
 end
