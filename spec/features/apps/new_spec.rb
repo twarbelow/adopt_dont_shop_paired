3de244c4d52d7d997acdb6ee0b59ebc35822ddf8
twarbelow <taija.warbelow@google.com>
2020-07-12 20:28:17 -0700

    create new_spec file for apps

---
+++ spec/features/apps/new_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe 'pet application' do
+  it 'allows a user to apply to adopt many pets' do
+  end
+  
+  it 'does not allow a user to apply if the form is not complete' do
+  end
+end
