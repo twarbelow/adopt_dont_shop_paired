5af991b4dadfbd03dbd6cd5cdb8fa799524a16de
twarbelow <taija.warbelow@google.com>
2020-07-13 18:49:47 -0700

    fix typo in successful flash

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -1,5 +1,21 @@
 class AppsController < ApplicationController
   def new
+  end
 
+  def create
+    new_app = App.new(app_params)
+    pets = params[:pet_ids].join(', ')
+    if new_app.save
+      flash[:notice] = "Application successfully submitted for #{pets}."
+      redirect_to "/favorites"
+    else
+      flash[:notice] = "Application not submitted: Required information missing."
+      render :new
+    end
+  end
+
+  private
+  def app_params
+    params.permit(:name, :address, :city, :state, :zip, :phone_number, :description)
   end
 end
