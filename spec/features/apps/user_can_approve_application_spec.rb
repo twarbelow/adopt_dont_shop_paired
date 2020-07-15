805d5a8a312baa70157f5c5bc28008e1a90cb957
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-07-15 14:59:24 -0600

    Update spec to in effort to locate error/bug

--- spec/features/apps/user_can_approve_application_spec.rb
+++ spec/features/apps/user_can_approve_application_spec.rb
@@ -2,22 +2,22 @@ require 'rails_helper'
 
 RSpec.describe 'As a visitor', type: :feature do
   before(:each) do
-    @shelter_1 = Shelter.create(name:        "Good Boys Are Us",
+    @shelter_1 = Shelter.create!(name:        "Good Boys Are Us",
                                  address:     "1234 ABC Street",
                                  city:        "Denver",
                                  state:       "Colorado",
                                  zip:         80202)
 
-    @pet_1 = Pet.create(image:               "https://www.vieravet.com/sites/default/files/styles/large/adaptive-image/public/golden-retriever-dog-breed-info.jpg?itok=LCRMRkum",
+    @pet_1 = Pet.create!(image:               "https://www.vieravet.com/sites/default/files/styles/large/adaptive-image/public/golden-retriever-dog-breed-info.jpg?itok=LCRMRkum",
                          name:                "Samson",
                          approximate_age:     4.5,
                          sex:                 "Male",
                          description:         "The best boy",
                          shelter_id:          @shelter_1.id,
-                         status:              "pending")
+                         status:              "adoptable")
 
 
-    @pet_2 = Pet.create(image:               "https://i.pinimg.com/736x/3f/47/7f/3f477ff92627ba171fcc867f23285cf5.jpg",
+    @pet_2 = Pet.create!(image:               "https://i.pinimg.com/736x/3f/47/7f/3f477ff92627ba171fcc867f23285cf5.jpg",
                          name:                "Delilah",
                          approximate_age:     4.0,
                          sex:                 "Female",
@@ -25,7 +25,7 @@ RSpec.describe 'As a visitor', type: :feature do
                          shelter_id:          @shelter_1.id,
                          status:              "adoptable")
 
-    @pet_3 = Pet.create(image:               "https://cdn0.wideopenpets.com/wp-content/uploads/2019/03/Untitled-design-2019-03-27T180734.510-770x405.jpg",
+    @pet_3 = Pet.create!(image:               "https://cdn0.wideopenpets.com/wp-content/uploads/2019/03/Untitled-design-2019-03-27T180734.510-770x405.jpg",
                          name:                "Sokka",
                          approximate_age:     3.0,
                          sex:                 "Male",
@@ -43,6 +43,8 @@ RSpec.describe 'As a visitor', type: :feature do
                                       })
 
     @application.pets << [@pet_1, @pet_2]
+
+    @favorites = Favorites.new([@pet_1.id, @pet_2.id])
 
   end
   describe "When a user visits an application's show page" do
@@ -65,7 +67,7 @@ RSpec.describe 'As a visitor', type: :feature do
         expect(current_path).to eq("/pets/#{@pet_2.id}")
 
         expect(page).to have_content "Status: pending"
-        save_and_open_page
+
         expect(page).to have_content "On hold for: #{@application.name}"
       end
     end
