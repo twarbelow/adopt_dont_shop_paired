381904fac274b01ed9263f006cbe0830e4b5edc4
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-07-15 15:01:35 -0600

    Update migrationg to set approved to default to false

--- db/migrate/20200715143331_add_approved_to_apps_pets.rb
+++ db/migrate/20200715143331_add_approved_to_apps_pets.rb
@@ -1,5 +1,5 @@
 class AddApprovedToAppsPets < ActiveRecord::Migration[5.1]
   def change
-    add_column :apps_pets, :approved, :boolean
+    add_column :apps_pets, :approved, :boolean, default: false
   end
 end
